45dbed1c641bb98d006d401ba145265edd90b54e
Gizaw <gizawag123@gmail.com>
2022-05-31 21:38:54 +0300

    creating search algorithms

--- temp.py
+++ temp.py
@@ -1,22 +1,93 @@
-import numpy as np
+import random
 
-landscape = np.random.randint(1, high=9, size=(10,10))
-print(landscape)
-start_state = (3, 6)  
-current_state = start_state
-count = 1
-ascending = True
-print ( landscape.shape)
+def randomSolution(tsp):
+    cities = list(range(len(tsp)))
+    solution = []
+    for i in range(len(tsp)):
+        randomCity = cities[random.randint(0, len(cities) - 1)]
+        solution.append(randomCity)
+        cities.remove(randomCity)
+    return solution
 
+def routeLength(tsp, solution):
+    routeLength = 0
+    for i in range(len(solution)):
+        routeLength += tsp[solution[i - 1]][solution[i]]
+    return routeLength
 
-# while ascending:
-#     print("\nStep #", count)
-#     print("Current state coordinates: ", current_state)
-#     print("Current state value: ", landscape[current_state[0]][current_state[1]])
-#     count += 1
-#     # ascending, current_state = hill_climb(current_state, landscape)
+def getNeighbours(solution):
 
-# print("\nStep #", count)
-# print("Optimization objective reached.")
-# print("Final state coordinates: ", current_state)
-# print("Final state value: ", landscape[current_state[0]][current_state[1]])
+    neighbours = []
+
+    for i in range(len(solution)):
+
+        for j in range(i + 1, len(solution)):
+
+            neighbour = solution.copy()
+
+            neighbour[i] = solution[j]
+
+            neighbour[j] = solution[i]
+
+            neighbours.append(neighbour)
+
+    return neighbours
+
+def getBestNeighbour(tsp, neighbours):
+
+    bestRouteLength = routeLength(tsp, neighbours[0])
+
+    bestNeighbour = neighbours[0]
+
+    for neighbour in neighbours:
+
+        currentRouteLength = routeLength(tsp, neighbour)
+
+        if currentRouteLength < bestRouteLength:
+
+            bestRouteLength = currentRouteLength
+
+            bestNeighbour = neighbour
+
+    return bestNeighbour, bestRouteLength
+
+def hillClimbing(tsp):
+
+    currentSolution = randomSolution(tsp)
+
+    currentRouteLength = routeLength(tsp, currentSolution)
+
+    neighbours = getNeighbours(currentSolution)
+
+    bestNeighbour, bestNeighbourRouteLength = getBestNeighbour(tsp, neighbours)
+
+    while bestNeighbourRouteLength < currentRouteLength:
+
+        currentSolution = bestNeighbour
+
+        currentRouteLength = bestNeighbourRouteLength
+
+        neighbours = getNeighbours(currentSolution)
+
+        bestNeighbour, bestNeighbourRouteLength = getBestNeighbour(tsp, neighbours)
+
+    return currentSolution, currentRouteLength
+
+def main():
+
+    tsp = [
+
+    [0, 400, 500, 300],
+
+    [400, 0, 300, 500],
+
+    [500, 300, 0, 400],
+
+    [300, 500, 400, 0]
+
+    ]
+
+    print(hillClimbing(tsp))
+
+if __name__ == "__main__":
+    main()
